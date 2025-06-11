e5d92363add8b79850d55310fe384419946d3765
dontstealmyusernameahhg <qwertyuiopreeyansh@gmail.com>
2025-06-11 20:10:48 +1200

    I Like

---
+++ while loop/homwACCI.py
@@ -0,0 +1,18 @@
+char = input("Enter a single character: ")
+
+if len(char) ==1:
+    acii_value = ord(char)
+    print(f"The ACII value is:")
+else:
+    print("Please enter a single character.")
+print(acii_value)
+# The ACII value is: 65 # The ACII value is: 97
+# The ACII value is: 48     # The ACII value is: 32
+# The ACII value is: 33     # The ACII value is: 64
+# The ACII value is: 36     # The ACII value is: 37
+# The ACII value is: 38     # The ACII value is: 39
+# The ACII value is: 40     # The ACII value is: 41
+# The ACII value is: 42     # The ACII value is: 43
+# The ACII value is: 44     # The ACII value is: 45
+# The ACII value is: 46     # The ACII value is: 47
+# The ACII value is: 58     # The ACII value is: 59
